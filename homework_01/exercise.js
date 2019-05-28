dbd1da014626588749bd7a711ac1de713487d1aa
Minh Phuong Tran <phuongtm1911@gmail.com>
2019-05-28 00:32:15 -0500

    Update js file

--- homework_01/exercise.js
+++ homework_01/exercise.js
@@ -1,15 +1,45 @@
+Array.prototype.removeNum = async function(num) {
+  newArr = this;
+  var removeNumber = function(num) { 
+    return new Promise(function(resolve, reject) {
+      if (newArr.includes(num, 0)) {
+        resolve(newArr.filter(n => n != num));
+      } else {
+        reject();
+      }
+    });
+  }
+  try {
+    newArr = await removeNumber(num);
+  } catch (error) {
+  } 
+  return newArr; 
+};
 
-Array.prototype.removeNum = function (num) {
-  setTimeout(function() {
-    this.filter(filterNum(num));
-  }, 1000);
+console.log('Start');
+console.log([1, 3, 4, 2, 1, 5].removeNum(1));
+console.log('Finish');
 
-  function filterNum(n)
-    return value != n;
+function promise() {
+  const p = fetch("https://randomuser.me/api/");
+  p.then(data => data.json())
+  .then(data => console.log(data))
+  .catch((err) => console.error(err));
+
+  
 }
 
-function run() {
-  console.log('Start');
-  console.log([1, 3, 4, 2, 1, 5].removeNum(1));
-  console.log('Finish');
+async function asyncFunc() {
+  try {
+    let result = await fetch("https://randomuser.me/api/");
+    console.log(result.json());
+  } catch (err) {
+    console.error(err);
+  } 
+}
+
+function reactjs() {
+  const { from } = rxjs;
+  p = fetch("https://randomuser.me/api/");
+  from(p).subscribe((data) => console.log(data.json()));
 }
