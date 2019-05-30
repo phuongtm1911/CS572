f227ed2c089dcc47d7dc95cf710720d997678bb4
Minh Phuong Tran <phuongtm1911@gmail.com>
2019-05-29 23:23:47 -0500

    update homework_03

--- homework_03/exercise3.js
+++ homework_03/exercise3.js
@@ -1,19 +1,35 @@
 var http = require('http');
+var url = require('url');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-    fs.readFileSync('big.file');
+    var pathname = url.parse(req.url).pathname;
+    if (pathname === '/readFileSync') {
+        readFileSync();
+    } else if (pathname === '/readFile') {
+        readFile();
+    } else if (pathname === '/readStream') {
+        readStream();
+    }
 }).listen(8080);
 
-http.createServer(function (req, res) {
-    fs.readFile('big.file', function(err, data) {
+function readFileSync() {
+    var data = fs.readFileSync('big.txt', 'utf8');
+    console.log(data);
+    console.log('Done readFileSync');
+}
+
+function readFile() {
+    fs.readFile('big.txt', 'utf8', function(err, data) {
         console.log(data);
     });
-}).listen(8080);
+    console.log('Done readFile');
+}
 
-http.createServer(function (req, res) {
-    var readable = fs.createReadStream('big.file');
+function readStream() {
+    var readable = fs.createReadStream('big.txt', 'utf8');
     readable.on('data', function(chunk) {
-        console.log(chunk.length);
+        console.log(chunk);
     });
-}).listen(8080);
+    console.log('Done readStream');
+}
