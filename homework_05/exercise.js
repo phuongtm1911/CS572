e07066da8878db61e6f1b9f122032ff17024f5d1
Minh Phuong Tran <50118891+phuongtm1911@users.noreply.github.com>
2019-06-01 09:17:43 -0500

    Update exercise.js

--- homework_05/exercise.js
+++ homework_05/exercise.js
@@ -5,17 +5,18 @@ var { shareReplay } = require('rxjs/operators');
 
 var app = express();
 
-app.get('*', function(req, res) {
-    var observer = from(axios.get('https://randomuser.me/api/?results=10; rel="last"'))
-        .pipe(shareReplay(1));
-    observer.subscribe(data => console.log('1st request'));
-    observer.subscribe(data => console.log('2nd request'));
-    res.end();
-});
-
-app.set('trust proxy', false);
-
+app.set('trust proxy', true);
 app.enable('case sensitive routing');
 app.set('strict routing', true);
+
+app.get('/users', function(req, res) {
+    var observer = from(axios.get('https://randomuser.me/api/?results=10;rel="first"'))
+        .pipe(shareReplay(1));
+    observer.subscribe(data => {
+        res.set("Cache-Control", "private, max-age=86400");
+        res.json(data);
+    });
+    res.end();
+});
 
 app.listen(8080);
