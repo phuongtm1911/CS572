4346edd0743acf14379298ae46e4008008a34857
Minh Phuong Tran <50118891+phuongtm1911@users.noreply.github.com>
2019-06-12 06:18:06 -0500

    Update app.component.ts

--- homework_11/app/app.component.ts
+++ homework_11/app/app.component.ts
@@ -4,18 +4,18 @@ import { CounterComponent } from './counter.component';
 
 @Component({
   selector: 'app-root',
-  template: `{{ComponentCounterValue}}
-  <app-counter [counter]="counterValue"
-       (counterChange)="this.ComponentCounterValue=$event"
-                ></app-counter>`,
-  styleUrls: ['./app.component.css']
+  template: `<div style="text-align:center">
+                <app-counter [counter]="ComponentCounterValue1"
+                            (counterChange)="ComponentCounterValue1=$event"></app-counter>
+                Component 1 Counter Value = {{ComponentCounterValue1}}
+                <app-counter [counter]="ComponentCounterValue2"
+                            (counterChange)="ComponentCounterValue2=$event"></app-counter>
+                Component 2 Counter Value = {{ComponentCounterValue2}}
+                </div>`,
+  styles: []
 })
+
 export class AppComponent {
-  counterValue: number = 5;
-  ComponentCounterValue: number;
-  
+  ComponentCounterValue1: number = 5;
+  ComponentCounterValue2: number = 5;
 }
-
-
-
-
